a92426416930b7b8a579cfedd2a3289fd2d6ac72
Khatamian <akhatami@d170802.stjude.sjcrh.local>
2018-08-15 14:04:33 -0500

    Added the expected output for the example data.

---
+++ data/output/sjaracne_TF_scripts_/03_getconsensusnetwork_BRCA100.sh
@@ -0,0 +1,1 @@
+/hpcf/apps/python/install/3.6.1/bin/python3 /research/rgs01/resgen/dev/wc/akhatami/gene_network/sjaracne_public/scripts/getconsensusnetwork.py data/sjaracne_BRCA100_out_/ 1e-05 data/sjaracne_BRCA100_out_.final/ >> data/sjaracne_BRCA100_log_/BRCA100_consensus_network.log
