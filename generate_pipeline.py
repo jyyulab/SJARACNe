bbd79b006d00ceb4056112722c0364619a44001b
Liang Ding <adamdingliang@gmail.com>
2019-01-17 15:12:47 -0600

    blackened

--- generate_pipeline.py
+++ generate_pipeline.py
@@ -1,136 +1,275 @@
 import os, sys
 
-SJARACNE_PATH = os.environ['SJARACNE_PATH']
-SJARACNE_PATH = SJARACNE_PATH if SJARACNE_PATH.endswith('/') else SJARACNE_PATH + '/'
-PYTHON_PATH = os.environ['PYTHON_PATH']
+SJARACNE_PATH = os.environ["SJARACNE_PATH"]
+SJARACNE_PATH = SJARACNE_PATH if SJARACNE_PATH.endswith("/") else SJARACNE_PATH + "/"
+PYTHON_PATH = os.environ["PYTHON_PATH"]
 sys.path.insert(0, SJARACNE_PATH)
 sys.path.insert(0, PYTHON_PATH)
 
-if sys.platform == 'linux':
-	sjaracne = 'sjaracne'
-elif sys.platform == 'darwin':
-	sjaracne = 'sjaracne.osx'
+if sys.platform == "linux":
+    sjaracne = "sjaracne"
+elif sys.platform == "darwin":
+    sjaracne = "sjaracne.osx"
 
 import argparse
 import numpy as np
 import shlex, subprocess
 
+
 def setup(args):
-	parser = argparse.ArgumentParser(description='SJARACNE package.')
-	parser.add_argument('project_name', help='Project name')
-	parser.add_argument('expression_matrix', help='Path to expression matrix, row indexes are used as the nodes in the network.')
-	parser.add_argument('hub_genes', help='Path to hub genes, containing list of symbols to be considered as hub genes.')
-	parser.add_argument('--bootstrap', type=int, default=100, help='Number of bootstrap networks.')
-	parser.add_argument('--c_threshold', type=float, default=float('1e-5'), help='P-value threshold in building consensus network.')
-	parser.add_argument('--p_threshold', type=float, default=float('1e-7'), help='P-value threshold in building bootstrap netwroks.')
-	parser.add_argument('--depth', type=int, default=40, help='Maximum partitioning depth.')
-	parser.add_argument('--run', type=bool, default=False, help='Whether run the pipeline or just generate and stop.')
-	parser.add_argument('--host', default='LOCAL', help='Whether to run on clusters or localhost. [LOCAL | CLUSTER]')
-	parser.add_argument('outdir', help='Output directory')
-	args_ = parser.parse_args(args[1:])
-	if args_.host == 'CLUSTER':
-		args_.run = 'False'
-	return args_
+    parser = argparse.ArgumentParser(description="SJARACNE package.")
+    parser.add_argument("project_name", help="Project name")
+    parser.add_argument(
+        "expression_matrix",
+        help="Path to expression matrix, row indexes are used as the nodes in the network.",
+    )
+    parser.add_argument(
+        "hub_genes",
+        help="Path to hub genes, containing list of symbols to be considered as hub genes.",
+    )
+    parser.add_argument(
+        "--bootstrap", type=int, default=100, help="Number of bootstrap networks."
+    )
+    parser.add_argument(
+        "--c_threshold",
+        type=float,
+        default=float("1e-5"),
+        help="P-value threshold in building consensus network.",
+    )
+    parser.add_argument(
+        "--p_threshold",
+        type=float,
+        default=float("1e-7"),
+        help="P-value threshold in building bootstrap netwroks.",
+    )
+    parser.add_argument(
+        "--depth", type=int, default=40, help="Maximum partitioning depth."
+    )
+    parser.add_argument(
+        "--run",
+        type=bool,
+        default=False,
+        help="Whether run the pipeline or just generate and stop.",
+    )
+    parser.add_argument(
+        "--host",
+        default="LOCAL",
+        help="Whether to run on clusters or localhost. [LOCAL | CLUSTER]",
+    )
+    parser.add_argument("outdir", help="Output directory")
+    args_ = parser.parse_args(args[1:])
+    if args_.host == "CLUSTER":
+        args_.run = "False"
+    return args_
+
 
 def setup_directory(out_dir, project_name):
-	boot_path = out_dir + 'sjaracne_' + project_name + '_out_/'
-	log_path = out_dir + 'sjaracne_' + project_name + '_log_/'
-	net_path = out_dir + 'sjaracne_' + project_name + '_out_.final/'
-	script_path = out_dir + 'sjaracne_' + project_name + '_scripts_/'
-	if not os.path.exists(boot_path):
-		os.mkdir(boot_path)
-	if not os.path.exists(log_path):
-		os.mkdir(log_path)
-	if not os.path.exists(net_path):
-		os.mkdir(net_path)
-	if not os.path.exists(script_path):
-		os.mkdir(script_path)
-	return [boot_path, log_path, net_path, script_path]
+    boot_path = out_dir + "sjaracne_" + project_name + "_out_/"
+    log_path = out_dir + "sjaracne_" + project_name + "_log_/"
+    net_path = out_dir + "sjaracne_" + project_name + "_out_.final/"
+    script_path = out_dir + "sjaracne_" + project_name + "_scripts_/"
+    if not os.path.exists(boot_path):
+        os.mkdir(boot_path)
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+    if not os.path.exists(net_path):
+        os.mkdir(net_path)
+    if not os.path.exists(script_path):
+        os.mkdir(script_path)
+    return [boot_path, log_path, net_path, script_path]
+
 
 def cleanup(args, paths):
-	out_0 = open(paths[3] + '00_cleanup_' + args.project_name + '.sh', 'w')
-	out_0.write('rm -rf ' + paths[0] + '\n')
-	out_0.write('rm -rf ' + paths[1] + '\n')
-	out_0.write('rm -rf ' + paths[2] + '\n')
-	out_0.write('rm -rf ' + paths[3] + '\n')
-	out_0.close()
+    out_0 = open(paths[3] + "00_cleanup_" + args.project_name + ".sh", "w")
+    out_0.write("rm -rf " + paths[0] + "\n")
+    out_0.write("rm -rf " + paths[1] + "\n")
+    out_0.write("rm -rf " + paths[2] + "\n")
+    out_0.write("rm -rf " + paths[3] + "\n")
+    out_0.close()
+
 
 def prep(args, paths):
-	out_1 = open(paths[3] + '01_prepare_' + args.project_name + '.sh', 'w')
-	out_1.write('perl -pe \'s/\\r\\n|\\n|\\r/\\n/g\' ' + args.hub_genes + ' > ' + args.hub_genes + '.tmp\n')
-	out_1.write('rm ' + args.hub_genes + '\n')
-	out_1.write('mv ' + args.hub_genes + '.tmp ' + args.hub_genes + '\n')
-	out_1.write('perl -pe \'s/\\r\\n|\\n|\\r/\\n/g\' ' + args.expression_matrix + ' > ' + args.expression_matrix + '.tmp\n')
-	out_1.write('rm ' + args.expression_matrix + '\n')
-	out_1.write('mv ' + args.expression_matrix + '.tmp ' + args.expression_matrix + '\n')
-	out_1.close()
+    out_1 = open(paths[3] + "01_prepare_" + args.project_name + ".sh", "w")
+    out_1.write(
+        "perl -pe 's/\\r\\n|\\n|\\r/\\n/g' "
+        + args.hub_genes
+        + " > "
+        + args.hub_genes
+        + ".tmp\n"
+    )
+    out_1.write("rm " + args.hub_genes + "\n")
+    out_1.write("mv " + args.hub_genes + ".tmp " + args.hub_genes + "\n")
+    out_1.write(
+        "perl -pe 's/\\r\\n|\\n|\\r/\\n/g' "
+        + args.expression_matrix
+        + " > "
+        + args.expression_matrix
+        + ".tmp\n"
+    )
+    out_1.write("rm " + args.expression_matrix + "\n")
+    out_1.write(
+        "mv " + args.expression_matrix + ".tmp " + args.expression_matrix + "\n"
+    )
+    out_1.close()
+
 
 def bootstrap(args, paths):
-	b = args.bootstrap + 1
-	out_2 = open(paths[3] + '02_bootstrap_' + args.project_name + '.sh', 'w')
-	for i in np.arange(1, b):
-		fname = paths[0] + args.project_name + '_run_' + str(i).zfill(int(np.log10(b)) + 1) + '.adj'
-		lname = paths[1] + args.project_name + '_run_' + str(i).zfill(int(np.log10(b)) + 1) + '.log'
-		arg = ' -i ' + args.expression_matrix + ' -l ' + args.hub_genes + ' -s ' + args.hub_genes + ' -p ' + str(args.p_threshold) + ' -e 0 -a adaptive_partitioning -r 1 -H ' + SJARACNE_PATH + 'config/ -N ' + str(args.depth)
-		script = SJARACNE_PATH + 'bin/' + sjaracne + ' ' + arg + ' -o ' + fname + ' -S ' + str(i) + ' '
-		if args.host == 'LOCAL':
-			script += ' >> ' + lname + ' &'
-		script += '\n'
-		out_2.write(script)
-	out_2.close()
+    b = args.bootstrap + 1
+    out_2 = open(paths[3] + "02_bootstrap_" + args.project_name + ".sh", "w")
+    for i in np.arange(1, b):
+        fname = (
+            paths[0]
+            + args.project_name
+            + "_run_"
+            + str(i).zfill(int(np.log10(b)) + 1)
+            + ".adj"
+        )
+        lname = (
+            paths[1]
+            + args.project_name
+            + "_run_"
+            + str(i).zfill(int(np.log10(b)) + 1)
+            + ".log"
+        )
+        arg = (
+            " -i "
+            + args.expression_matrix
+            + " -l "
+            + args.hub_genes
+            + " -s "
+            + args.hub_genes
+            + " -p "
+            + str(args.p_threshold)
+            + " -e 0 -a adaptive_partitioning -r 1 -H "
+            + SJARACNE_PATH
+            + "config/ -N "
+            + str(args.depth)
+        )
+        script = (
+            SJARACNE_PATH
+            + "bin/"
+            + sjaracne
+            + " "
+            + arg
+            + " -o "
+            + fname
+            + " -S "
+            + str(i)
+            + " "
+        )
+        if args.host == "LOCAL":
+            script += " >> " + lname + " &"
+        script += "\n"
+        out_2.write(script)
+    out_2.close()
+
 
 def consensus(args, paths):
-	out_3 = open(paths[3] + '03_getconsensusnetwork_' + args.project_name + '.sh', 'w')
-	script = PYTHON_PATH + ' ' + SJARACNE_PATH + 'getconsensusnetwork.py ' + paths[0] + ' ' + str(args.c_threshold) + ' ' + paths[2] + ' '
-	if args.host == 'LOCAL':
-		script += ' >> ' + paths[1] + args.project_name + '_consensus_network.log'
-	script += '\n'
-	out_3.write(script)
-	out_3.close()
+    out_3 = open(paths[3] + "03_getconsensusnetwork_" + args.project_name + ".sh", "w")
+    script = (
+        PYTHON_PATH
+        + " "
+        + SJARACNE_PATH
+        + "getconsensusnetwork.py "
+        + paths[0]
+        + " "
+        + str(args.c_threshold)
+        + " "
+        + paths[2]
+        + " "
+    )
+    if args.host == "LOCAL":
+        script += " >> " + paths[1] + args.project_name + "_consensus_network.log"
+    script += "\n"
+    out_3.write(script)
+    out_3.close()
+
 
 def enhanced(args, paths):
-	out_4 = open(paths[3] + '04_getenhancedconsensusnetwork_' + args.project_name + '.sh', 'w')
-	script = PYTHON_PATH + ' ' + SJARACNE_PATH + 'getenhancedconsensusnetwork.py ' + args.expression_matrix + ' ' + paths[2] + 'consensus_network_3col_.txt ' + paths[2] + ' '
-	if args.host == 'LOCAL':
-		script += ' >> ' + paths[1] + args.project_name + '_enhanced_network.log'
-	script += '\n'
-	out_4.write(script)
-	out_4.close()
+    out_4 = open(
+        paths[3] + "04_getenhancedconsensusnetwork_" + args.project_name + ".sh", "w"
+    )
+    script = (
+        PYTHON_PATH
+        + " "
+        + SJARACNE_PATH
+        + "getenhancedconsensusnetwork.py "
+        + args.expression_matrix
+        + " "
+        + paths[2]
+        + "consensus_network_3col_.txt "
+        + paths[2]
+        + " "
+    )
+    if args.host == "LOCAL":
+        script += " >> " + paths[1] + args.project_name + "_enhanced_network.log"
+    script += "\n"
+    out_4.write(script)
+    out_4.close()
+
 
 def pipeline(args, paths):
-	out_0 = open(paths[3] + '00_pipeline_' + args.project_name + '.sh', 'w')
-	#script = 'sh ' + paths[3] + '00_cleanup_' + args.project_name + '.sh\n'
-	#out_0.write(script)
-	#script = 'sh ' + paths[3] + '01_prepare_' + args.project_name + '.sh\n'
-	#out_0.write(script)
-	script = 'sh ' + paths[3] + '02_bootstrap_' + args.project_name + '.sh\n'
-	out_0.write(script)
-	out_0.write('jobs=$(ps -ef | grep \"' + args.project_name + '\" | grep \"' + sjaracne + ' -i\" -c)\n')
-	out_0.write('while [ $jobs -gt 0 ]\ndo\n\tsleep 30\n\tjobs=$(ps -ef | grep \"' + args.project_name + '\" | grep \"' + sjaracne + ' -i\" -c)\ndone\n')
-	script = 'sh ' + paths[3] + '03_getconsensusnetwork_' + args.project_name + '.sh\n'
-	out_0.write(script)
-	out_0.write('jobs=$(ps -ef | grep \"' + args.project_name + '\" | grep getconsensusnetwork -c)\n')
-	out_0.write('while [ $jobs -gt 0 ]\ndo\n\tsleep 30\n\tjobs=$(ps -ef | grep \"' + args.project_name + '\" | grep getconsensusnetwork -c)\ndone\n')
-	script = 'sh ' + paths[3] + '04_getenhancedconsensusnetwork_' + args.project_name + '.sh\n'
-	out_0.write(script)
-	out_0.close()
+    out_0 = open(paths[3] + "00_pipeline_" + args.project_name + ".sh", "w")
+    # script = 'sh ' + paths[3] + '00_cleanup_' + args.project_name + '.sh\n'
+    # out_0.write(script)
+    # script = 'sh ' + paths[3] + '01_prepare_' + args.project_name + '.sh\n'
+    # out_0.write(script)
+    script = "sh " + paths[3] + "02_bootstrap_" + args.project_name + ".sh\n"
+    out_0.write(script)
+    out_0.write(
+        'jobs=$(ps -ef | grep "'
+        + args.project_name
+        + '" | grep "'
+        + sjaracne
+        + ' -i" -c)\n'
+    )
+    out_0.write(
+        'while [ $jobs -gt 0 ]\ndo\n\tsleep 30\n\tjobs=$(ps -ef | grep "'
+        + args.project_name
+        + '" | grep "'
+        + sjaracne
+        + ' -i" -c)\ndone\n'
+    )
+    script = "sh " + paths[3] + "03_getconsensusnetwork_" + args.project_name + ".sh\n"
+    out_0.write(script)
+    out_0.write(
+        'jobs=$(ps -ef | grep "'
+        + args.project_name
+        + '" | grep getconsensusnetwork -c)\n'
+    )
+    out_0.write(
+        'while [ $jobs -gt 0 ]\ndo\n\tsleep 30\n\tjobs=$(ps -ef | grep "'
+        + args.project_name
+        + '" | grep getconsensusnetwork -c)\ndone\n'
+    )
+    script = (
+        "sh "
+        + paths[3]
+        + "04_getenhancedconsensusnetwork_"
+        + args.project_name
+        + ".sh\n"
+    )
+    out_0.write(script)
+    out_0.close()
+
 
 def run(args):
-	args_ = setup(args)
-	paths = setup_directory(args_.outdir, args_.project_name)
-	cleanup(args_, paths)
-	prep(args_, paths)
-	bootstrap(args_, paths)
-	consensus(args_, paths)
-	enhanced(args_, paths)
-	pipeline(args_, paths)
-	if args_.run == True:
-		script = 'sh ' + paths[3] + '00_pipeline_' + args_.project_name + '.sh '
-		if args_.host == 'LOCAL':
-			script += ' >> ' + paths[1] + args_.project_name + '_pipeline.log'
-		script += '\n'
-		subprocess.Popen(shlex.split(script))
+    args_ = setup(args)
+    paths = setup_directory(args_.outdir, args_.project_name)
+    cleanup(args_, paths)
+    prep(args_, paths)
+    bootstrap(args_, paths)
+    consensus(args_, paths)
+    enhanced(args_, paths)
+    pipeline(args_, paths)
+    if args_.run == True:
+        script = "sh " + paths[3] + "00_pipeline_" + args_.project_name + ".sh "
+        if args_.host == "LOCAL":
+            script += " >> " + paths[1] + args_.project_name + "_pipeline.log"
+        script += "\n"
+        subprocess.Popen(shlex.split(script))
 
-if __name__ == '__main__':
-	run(sys.argv)
-	print('[INFO] --> [ARCN] Finished.')
+
+if __name__ == "__main__":
+    run(sys.argv)
+    print("[INFO] --> [ARCN] Finished.")
--- getconsensusnetwork.py
+++ getconsensusnetwork.py
@@ -1,9 +1,9 @@
 import math, sys, os, tarfile, shutil
 from contextlib import closing
 
-SJARACNE_PATH = os.environ['SJARACNE_PATH']
-SJARACNE_PATH = SJARACNE_PATH if SJARACNE_PATH.endswith('/') else SJARACNE_PATH + '/'
-PYTHON_PATH = os.environ['PYTHON_PATH']
+SJARACNE_PATH = os.environ["SJARACNE_PATH"]
+SJARACNE_PATH = SJARACNE_PATH if SJARACNE_PATH.endswith("/") else SJARACNE_PATH + "/"
+PYTHON_PATH = os.environ["PYTHON_PATH"]
 sys.path.insert(0, SJARACNE_PATH)
 sys.path.insert(0, PYTHON_PATH)
 
@@ -18,86 +18,141 @@ total_spearman = {}
 total_regression = {}
 total_pvalue = {}
 symbol_set = {}
-parameters = ''
-header = ''
+parameters = ""
+header = ""
 run_num = 0
 mu = 0
 sigma = 0
 
 # Processing all bootstrap networks, summarizing them into corresponding variables
 for adj_file in os.listdir(sys.argv[1]):
-	total_edge.append(0)
-	with open(sys.argv[1] + adj_file, 'r') as f:	# Opening each bootstrap file
-		for line in f:
-			if line[0] == '>' and run_num == 0:
-				parameters += line
-			if line[0] != '>':			# Processing non header lines representing the network 
-				tokens = line.split('\t')	# Tokenizing each non header line with tab delimmiter
-				hub_id = tokens[0]		# First token is the hub id
-				for i in range(1,len(tokens),2):	# Iterating on all adjacent genes: Odd indexes are the connected genes and even indexes are the corresponding value to the edge between the hub gene and the gene with an odd index appearing before the value in the tokens list
-					key = hub_id + '--' + tokens[i]	# Creating a key for the edge
-					if key in total_support:	
-						total_support[key] += 1	# Updating the total number of edges observed for the particualr key (edge)
-						total_mi[key] += float(tokens[i+1])	# Updating total MI between the genes involving in the particular key (edge)
-					else:
-						total_support[key] = 1	# Initializing the total number of edges observed for the particular key (edge) if the key (edge) is newly generated
-						total_mi[key] = float(tokens[i+1])	# Initializing total MI between the genes involving in the particulat key (edge) if the key (edge) is newly generated
-					total_edge[run_num] += 1	# Increment the total number of edges processed so far
-	run_num += 1	# Increment the bootstrap file index
+    total_edge.append(0)
+    with open(sys.argv[1] + adj_file, "r") as f:  # Opening each bootstrap file
+        for line in f:
+            if line[0] == ">" and run_num == 0:
+                parameters += line
+            if line[0] != ">":  # Processing non header lines representing the network
+                tokens = line.split(
+                    "\t"
+                )  # Tokenizing each non header line with tab delimmiter
+                hub_id = tokens[0]  # First token is the hub id
+                for i in range(
+                    1, len(tokens), 2
+                ):  # Iterating on all adjacent genes: Odd indexes are the connected genes and even indexes are the corresponding value to the edge between the hub gene and the gene with an odd index appearing before the value in the tokens list
+                    key = hub_id + "--" + tokens[i]  # Creating a key for the edge
+                    if key in total_support:
+                        total_support[
+                            key
+                        ] += (
+                            1
+                        )  # Updating the total number of edges observed for the particualr key (edge)
+                        total_mi[key] += float(
+                            tokens[i + 1]
+                        )  # Updating total MI between the genes involving in the particular key (edge)
+                    else:
+                        total_support[
+                            key
+                        ] = (
+                            1
+                        )  # Initializing the total number of edges observed for the particular key (edge) if the key (edge) is newly generated
+                        total_mi[key] = float(
+                            tokens[i + 1]
+                        )  # Initializing total MI between the genes involving in the particulat key (edge) if the key (edge) is newly generated
+                    total_edge[
+                        run_num
+                    ] += 1  # Increment the total number of edges processed so far
+    run_num += 1  # Increment the bootstrap file index
 
 # Network name
-path_tokens = sys.argv[1].split('/')
+path_tokens = sys.argv[1].split("/")
 name = path_tokens[len(path_tokens) - 2]
-path = '/'.join(path_tokens[0:len(path_tokens) - 2]) + '/'
+path = "/".join(path_tokens[0 : len(path_tokens) - 2]) + "/"
 
 # Archiving the bootstrap networks
 current_path = os.getcwd()
 os.chdir(path)
-with closing(tarfile.open(name + '.tar.gz', "w:gz")) as tar:
-	for f in os.listdir(name):
-	    tar.add(name + '/' + f)
+with closing(tarfile.open(name + ".tar.gz", "w:gz")) as tar:
+    for f in os.listdir(name):
+        tar.add(name + "/" + f)
 os.chdir(current_path)
 shutil.rmtree(sys.argv[1])
 
 # Writing out the summary of all bootstrap files into bootstrap_info.txt file
-info_file = open(sys.argv[3] + 'bootstrap_info_.txt' if sys.argv[3].endswith('/') else sys.argv[3] + '/bootstrap_info_.txt', 'w')
-info_file.write('Total edge tested: ' + str(len(total_support)) + '\n')
-info_file.write('Bonferroni corrected (0.05) alpha: ' + str(0.05/len(total_support)) + '\n')
+info_file = open(
+    sys.argv[3] + "bootstrap_info_.txt"
+    if sys.argv[3].endswith("/")
+    else sys.argv[3] + "/bootstrap_info_.txt",
+    "w",
+)
+info_file.write("Total edge tested: " + str(len(total_support)) + "\n")
+info_file.write(
+    "Bonferroni corrected (0.05) alpha: " + str(0.05 / len(total_support)) + "\n"
+)
 
 # Computing mu and sigma accross all bootstrap files
 for i in range(0, run_num):
-	prob = float(total_edge[i]) / float(len(total_support))
-	mu += prob
-	sigma += prob * (1 - prob)
+    prob = float(total_edge[i]) / float(len(total_support))
+    mu += prob
+    sigma += prob * (1 - prob)
 sigma = math.sqrt(sigma)
 
-info_file.write('mu: ' + str(mu) + '\n')
-info_file.write('sigma: ' + str(sigma) + '\n')
+info_file.write("mu: " + str(mu) + "\n")
+info_file.write("sigma: " + str(sigma) + "\n")
 
 # Setting p_threshold if given to the given value, if not to Bonferroni corrected value
 p_threshold = 0.05 / len(total_support)
 if sys.argv[2] != None:
-	p_threshold = float(sys.argv[2])
+    p_threshold = float(sys.argv[2])
 
 # Writing out the parameters that the bootstrap networks are constructed with plus other parameters that is used to create consensus network
-parameter_file = open(sys.argv[3] + 'parameter_info_.txt' if sys.argv[3].endswith('/') else sys.argv[3] + '/parameter_info_.txt', 'w')
-parameters += '>  Bootstrap No: ' + str(run_num) + '\n'
-parameters += '>  Source: sjaracne\n'
-parameters += '>  Output network: ' + (sys.argv[3] + 'consensus_network_3col_.txt' if sys.argv[3].endswith('/') else sys.argv[3] + '/consensus_network_3col_.txt') + '\n'
+parameter_file = open(
+    sys.argv[3] + "parameter_info_.txt"
+    if sys.argv[3].endswith("/")
+    else sys.argv[3] + "/parameter_info_.txt",
+    "w",
+)
+parameters += ">  Bootstrap No: " + str(run_num) + "\n"
+parameters += ">  Source: sjaracne\n"
+parameters += (
+    ">  Output network: "
+    + (
+        sys.argv[3] + "consensus_network_3col_.txt"
+        if sys.argv[3].endswith("/")
+        else sys.argv[3] + "/consensus_network_3col_.txt"
+    )
+    + "\n"
+)
 parameter_file.write(parameters)
 
 # Writing out the consensus network preserving edges with statistically significant support
-consensus_network = open(sys.argv[3] + 'consensus_network_3col_.txt' if sys.argv[3].endswith('/') else sys.argv[3] + '/consensus_network_3col_.txt', 'w')
-header += 'source\ttarget\tMI\n'
+consensus_network = open(
+    sys.argv[3] + "consensus_network_3col_.txt"
+    if sys.argv[3].endswith("/")
+    else sys.argv[3] + "/consensus_network_3col_.txt",
+    "w",
+)
+header += "source\ttarget\tMI\n"
 consensus_network.write(header)
-current_gene = 'none'
+current_gene = "none"
 
-for key in sorted(total_support.keys()):	# Iterating on all edges in a sorted fashion
-	gene1 = key.split('--')[0]		# Extracting first gene involving in an edge from the key (edge)
-	gene2 = key.split('--')[1]		# Extracting second gene involving in an edge from the key (edge)
-	z = float(total_support[key] - mu) / float(sigma) if sigma != 0 else 100	# Computing the z score of normal distribution 
-	pval = statistics.uprob(z)		# Computing p-value corresponding to the z score --> Implemented in statistics.py module inspired by Statistics::Distributions::uprob function in perl
-	if pval < p_threshold:			# Decision making if the edge has enough support or not and therefore if it has to be remained or removed
-		mi = '{0:.4f}'.format(float(total_mi[key]) / float(total_support[key]))	# Computing MI corresponding to an edge remaining in the network
-		consensus_network.write(gene1 + '\t' + gene2 + '\t' + mi + '\n')
+for key in sorted(total_support.keys()):  # Iterating on all edges in a sorted fashion
+    gene1 = key.split("--")[
+        0
+    ]  # Extracting first gene involving in an edge from the key (edge)
+    gene2 = key.split("--")[
+        1
+    ]  # Extracting second gene involving in an edge from the key (edge)
+    z = (
+        float(total_support[key] - mu) / float(sigma) if sigma != 0 else 100
+    )  # Computing the z score of normal distribution
+    pval = statistics.uprob(
+        z
+    )  # Computing p-value corresponding to the z score --> Implemented in statistics.py module inspired by Statistics::Distributions::uprob function in perl
+    if (
+        pval < p_threshold
+    ):  # Decision making if the edge has enough support or not and therefore if it has to be remained or removed
+        mi = "{0:.4f}".format(
+            float(total_mi[key]) / float(total_support[key])
+        )  # Computing MI corresponding to an edge remaining in the network
+        consensus_network.write(gene1 + "\t" + gene2 + "\t" + mi + "\n")
 consensus_network.close()
