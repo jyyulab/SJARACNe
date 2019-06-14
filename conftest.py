df82bec3436335ac427c46b2fc05478fa7413841
Jimmy Veloso <joaopvapv100@yahoo.com.br>
2019-06-14 15:57:24 -0500

    Changing script function and relative paths of unit tests

---
+++ conftest.py
@@ -0,0 +1,4 @@
+'''
+This file makes sure that testpy will check the main directory and find all modules in this program.
+This file is REQUIRED for Travis CI integration testing.
+'''
--- tests/test_create_consensus_network.py
+++ tests/test_create_consensus_network.py
@@ -6,31 +6,41 @@ import sys
 import os
 import shutil
 from SJARACNe.bin.create_consensus_network import create_consensus_network as cn
+from SJARACNe.bin.create_consensus_network import create_enhanced_consensus_network as ecn
 from SJARACNe.bin.create_consensus_network import uprob
 
 class TestConsensusNetwork(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        os.mkdir('./test_answers')
-        cn('inputs/adjmat_dir', 0.05, './test_answers')
+        self.path = './test_answers'
+        if self.path is None:
+            os.mkdir('./test_answers')
+        cn('./tests/inputs/adjmat_dir', 0.05, './test_answers')
 
     
     def test_consensus_network_3col(self):
-        self.assertTrue(filecmp.cmp('./answerkey/consensus_network_3col_.txt', './test_answers/consensus_network_3col_.txt'))
+        self.assertTrue(filecmp.cmp('./tests/answerkey/consensus_network_3col_.txt', './test_answers/consensus_network_3col_.txt'))
         
     def test_bootstrap_info(self):
-        self.assertTrue(filecmp.cmp('./answerkey/bootstrap_info_.txt', './test_answers/bootstrap_info_.txt'))
+        self.assertTrue(filecmp.cmp('./tests/answerkey/bootstrap_info_.txt', './test_answers/bootstrap_info_.txt'))
 
     def test_parameter_info(self):  
         #delete last line of parameter info, as it will not match with answer key
-        with open('./test_answers/parameter_info_.txt', 'r') as info:
+        with open('./tests/test_answers/parameter_info_.txt', 'r') as info:
             lines = info.readlines()
             lines = lines[:-1]
             info.close()
         with open('./test_answers/parameter_info_.txt', 'w') as info:
             info.writelines(lines)
             info.close()
-        self.assertTrue(filecmp.cmp('./answerkey/parameter_info_.txt', './test_answers/parameter_info_.txt'))
+        self.assertTrue(filecmp.cmp('./tests/answerkey/parameter_info_.txt', './test_answers/parameter_info_.txt'))
+
+
+    '''
+    def test_enhanced_consensus_network(self):
+        ecn('./inputs/BRCA100.exp', './answerkey/consensus_network_3col_.txt', './test_answers/consensus_network_ncol_.txt')
+        self.assertTrue(filecmp.cmp('./answerkey/consensus_network_ncol_.txt', './test_answers/consensus_network_ncol_.txt'))
+    '''
 
     #Testing different values on the function uprob(z-score)
     def test_uprob_100(self):
