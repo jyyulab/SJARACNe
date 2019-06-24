42ed838412683cd87ebe24df4cd13a264b6bfe8b
Jimmy Veloso <joaopvapv100@yahoo.com.br>
2019-06-24 15:14:53 -0500

    Further reduced adjacency matrices to n=5

--- tests/test_sjaracne_acceptance.py
+++ tests/test_sjaracne_acceptance.py
@@ -7,10 +7,10 @@ import subprocess
 class TestSJARACNe(unittest.TestCase):
     def test_acceptance(self):
         with tempfile.TemporaryDirectory() as tmpdir:
-            cmd = 'sjaracne local -e ./tests/inputs/Tcell1170.exp -g ./tests/inputs/TcellTF.txt -n 10 -o {}'.format(tmpdir)
+            cmd = 'sjaracne local -e ./tests/inputs/Tcell1170.exp -g ./tests/inputs/TcellTF.txt -n 5 -o {}'.format(tmpdir)
             exe = shlex.split(cmd)
             subprocess.check_call(exe)
-            self.assertTrue(filecmp.cmp('./tests/answerkey/acceptance/cnn_10.txt', '{}/consensus_network_ncol_.txt'.format(tmpdir)))
+            self.assertTrue(filecmp.cmp('./tests/answerkey/acceptance/cnn_5.txt', '{}/consensus_network_ncol_.txt'.format(tmpdir)))
 
 if __name__ == '__main__':
     unittest.main()
