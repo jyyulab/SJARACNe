e7c4e0edaa47d5b7a3287b121b969940e7a76723
Jimmy Veloso <joaopvapv100@yahoo.com.br>
2019-06-24 16:36:06 -0500

    necessary Travis debugging

--- tests/test_sjaracne_acceptance.py
+++ tests/test_sjaracne_acceptance.py
@@ -10,6 +10,9 @@ class TestSJARACNe(unittest.TestCase):
             cmd = 'sjaracne local -e ./tests/inputs/Tcell1170.exp -g ./tests/inputs/TcellTF.txt -n 5 -o {}'.format(tmpdir)
             exe = shlex.split(cmd)
             subprocess.check_call(exe)
+            cmd2 = 'diff ./tests/answerkey/acceptance/cnn_5.txt {}/consensus_network_ncol_.txt'.format(tmpdir)
+            exe2 = shlex.split(cmd2)
+            subprocess.check_call(exe2)
             self.assertTrue(filecmp.cmp('./tests/answerkey/acceptance/cnn_5.txt', '{}/consensus_network_ncol_.txt'.format(tmpdir)))
 
 if __name__ == '__main__':
