1d783d61a41ca567b67261135d3813502276930d
Jimmy Veloso <joaopvapv100@yahoo.com.br>
2019-06-24 11:44:59 -0500

    Using temporary directory for acceptance testing

--- tests/test_sjaracne_acceptance.py
+++ tests/test_sjaracne_acceptance.py
@@ -6,10 +6,11 @@ import subprocess
 
 class TestSJARACNe(unittest.TestCase):
     def test_acceptance(self):
-        cmd = 'sjaracne local -e ./tests/inputs/Tcell1170.exp -g ./tests/inputs/TcellTF.txt -o ./tests/test_run'
-        exe = shlex.split(cmd)
-        subprocess.check_call(exe)
-        self.assertTrue(filecmp.cmp('./tests/answerkey/acceptance/consensus_network_ncol_.txt', './tests/test_run/consensus_network_ncol_.txt'))
+        with tempfile.TemporaryDirectory() as tmpdir:
+            cmd = 'sjaracne local -e ./tests/inputs/Tcell1170.exp -g ./tests/inputs/TcellTF.txt -o {}'.format(tmpdir)
+            exe = shlex.split(cmd)
+            subprocess.check_call(exe)
+            self.assertTrue(filecmp.cmp('./tests/answerkey/acceptance/consensus_network_ncol_.txt', '{}/consensus_network_ncol_.txt'.format(tmpdir)))
 
 if __name__ == '__main__':
     unittest.main()
