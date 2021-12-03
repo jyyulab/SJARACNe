c03c5e4d4ac2952224e2fc872868c062f4745cb7
Liang Ding <adamdingliang@gmail.com>
2021-12-02 18:04:03 -0600

    add -pc to acceptance test

--- tests/test_sjaracne_acceptance.py
+++ tests/test_sjaracne_acceptance.py
@@ -10,7 +10,7 @@ import subprocess
 class TestSJARACNe(unittest.TestCase):
     def test_acceptance(self):
         with tempfile.TemporaryDirectory() as tmpdir:
-            cmd = 'sjaracne local -e ./tests/inputs/Tcell1170.exp -g ./tests/inputs/TcellTF.txt -n 5 -o {}'.format(tmpdir)
+            cmd = 'sjaracne local -e ./tests/inputs/Tcell1170.exp -g ./tests/inputs/TcellTF.txt -n 5 -pc 0.01 -o {}'.format(tmpdir)
             exe = shlex.split(cmd)
             subprocess.check_call(exe)
             cmd2 = 'diff ./tests/answerkey/acceptance/cnn_5.txt {}/consensus_network_ncol_.txt'.format(tmpdir)
